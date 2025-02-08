d01d5f1b7d3fd0cc7a2d9faf9badcfa04a602a28
MaximusTaillon <157655771+MaximusTaillon@users.noreply.github.com>
2025-02-08 16:23:27 -0500

    Test ligne 33.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -30,7 +30,7 @@ const Index = () => {
         <div className="container mx-auto px-4 z-10">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
+            animate={{ opacity: 10, y: 0 }}
             transition={{ duration: 0.8 }}
             className="text-center"
           >
