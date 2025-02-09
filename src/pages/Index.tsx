971b85502d32b354ceaee3f1734366c2c49d116a
MaximusTaillon <157655771+MaximusTaillon@users.noreply.github.com>
2025-02-09 02:43:24 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -125,7 +125,7 @@ const Index = () => {
             
             <div className="space-y-4">
               <div className="flex justify-between text-white">
-                <span>2025</span>
+                <span>Année</span>
                 <span>{annee[0]}</span>
                 <span>2100</span>
               </div>
