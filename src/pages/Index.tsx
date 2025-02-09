f0776d0a7308186ce48c68ff24e61283e1435277
MaximusTaillon <157655771+MaximusTaillon@users.noreply.github.com>
2025-02-09 02:20:23 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -124,14 +124,14 @@ const Index = () => {
             <div className="space-y-4">
               <div className="flex justify-between text-white">
                 <span>Année</span>
-                <span>{seaLevel[0]+2025}</span>
+                <span>{seaLevel[0]}</span>
               </div>
               <Slider
                 defaultValue={2025}
                 max={2100}
                 step={1}
-                value={seaLevel+2025}
-                onValueChange={setSeaLevel+2025}
+                value={seaLevel}
+                onValueChange={setSeaLevel}
                 className="py-4"
               />
             </div>
